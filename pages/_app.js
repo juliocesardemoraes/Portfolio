901a783a08b38782bd7b5c4d3c16c43b73718e35
julio cesar <jcmcf2012@gmail.com>
2022-03-16 07:31:49 -0300

    feat: minor eslint changes and adding experience page

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/provider";
-import Layout from '../components/layouts/main'
+import Layout from "../components/layouts/main";
 import { AnimatePresence } from "framer-motion";
-import Fonts from '../components/fonts'
-import theme from '../lib/theme'
+import Fonts from "../components/fonts";
+import theme from "../lib/theme";
 
-const Website = ({ Component, pageProps, router}) => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Fonts></Fonts>
-            <Layout router={router}>
-                <AnimatePresence exitBeforeEnter initial={true}>
-                    <Component {...pageProps} key={router.route} />
-                </AnimatePresence>
-            </Layout>
-        </ChakraProvider>
-    )
-}
+const Website = ({ Component, pageProps, router }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts></Fonts>
+      <Layout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  );
+};
 
-export default Website
+export default Website;
---
+++ pages/experiencia.js
@@ -0,0 +1,86 @@
+import { Heading, Button, Box, chakra, Img } from "@chakra-ui/react";
+import { Link } from "next/link";
+import { AnimatePresence, motion } from "framer-motion";
+import Layout from "../components/layouts/article";
+import styled from "@emotion/styled";
+
+const ProjectImage = chakra(Img, {
+  shouldForwardProp: (prop) =>
+    ["width", "height", "src", "alt", "target"].includes(prop),
+});
+
+const ProjectDiv = styled.span`
+  img {
+    transition: 200ms ease;
+  }
+  &:hover img {
+    transform: scale(1.25);
+  }
+`;
+
+const Projetos = () => (
+  <Layout title="Projetos">
+    <Box textAlign={"center"}>
+      <Heading as="h2" variant="page-title">
+        Projetos
+      </Heading>
+    </Box>
+    <Box background={"#fff"} borderRadius={"1em"} mt={"1em"}>
+      <Box display={"flex"} flexDirection={"row"}>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          padding={4}
+          justifyContent={"space-around"}
+        >
+          <Box>
+            <Heading as="h1" color={"#2A2A2C"}>
+              Aprendendo Codando
+            </Heading>
+            <Heading as="h5" fontSize="12px" fontWeight={300} color={"#2A2A2C"}>
+              Website e-learning
+            </Heading>
+          </Box>
+          {/* <ProjectImage
+            src="/images/isometricModel.png"
+            alt="logo do e-learning"
+            borderRadius="1em"
+            width="100px"
+            height="100px"
+          ></ProjectImage>*/}
+          <a
+            href="https://aprendendo-codando.vercel.app/"
+            scroll={false}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button
+              mt={12}
+              colorScheme="blue"
+              href="https://aprendendo-codando.vercel.app/"
+            >
+              Me mostre mais
+            </Button>
+          </a>
+        </Box>
+        <ProjectDiv ml={4}>
+          <a
+            href="https://aprendendo-codando.vercel.app/"
+            scroll={false}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ProjectImage
+              src="/images/AprendendoCodando.png"
+              alt="imagem do website do portfolio"
+              width="100%"
+              height="100%"
+            ></ProjectImage>
+          </a>
+        </ProjectDiv>
+      </Box>
+    </Box>
+  </Layout>
+);
+
+export default Projetos;
