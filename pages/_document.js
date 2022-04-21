1729a01e180d200655098fc3711623bd110e9908
Julio Cesar <jcmcf2012@gmail.com>
2022-04-21 09:41:27 -0300

    feat: adding meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,25 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import theme from "../lib/theme";
+const imageScreenshot = `/images/PortfolioScreenshot.png`;
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head prefix="">
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta property="og:title" content="Portfolio Main Page" />
+          <meta property="og:image" content={imageScreenshot} />
+          <meta property="og:description" content="Portfolio land page" />
+          <meta
+            property="og:url"
+            content="https://portfolio-red-eta.vercel.app/images/PortfolioScreenshot.png"
+          />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
